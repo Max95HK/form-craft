63ea86e4c2183533d37027f17fcc0e078da6ae16
Max <massimo.musso.95@gmail.com>
2026-07-01 20:17:54 +0200

    Fatto roba

--- src/components/field-previews/checkbox-field-preview-component.tsx
+++ src/components/field-previews/checkbox-field-preview-component.tsx
@@ -1,11 +1,19 @@
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Checkbox } from "@/components/ui/checkbox"
+
 import type { CheckboxFieldConfig } from "@/types";
 
 type CheckboxFieldPreviewCompProps = {
   config: CheckboxFieldConfig;
 };
 
-const CheckboxFieldPreviewComp = ({ config }: CheckboxFieldPreviewCompProps) => {
-  return <div>Preview</div>;
+const CheckboxFieldPreviewComp = ({ config: { label, id, defaultValue,  }, }: CheckboxFieldPreviewCompProps) => {
+  return (
+    <Field>
+      <FieldLabel htmlFor={id}>{label}</FieldLabel>
+      <Checkbox id={id} checked={defaultValue}/>
+    </Field>
+  );
 };
 
 export default CheckboxFieldPreviewComp;
