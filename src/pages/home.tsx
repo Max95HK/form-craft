bc02f71cc98907c5bc83625d126f1ba8ba4cdecc
Max <massimo.musso.95@gmail.com>
2026-07-01 22:00:40 +0200

    Fatto roba

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,6 +7,7 @@ import EmptyState from "@/components/empty-state";
 import SortableField from "@/components/sortable-field";
 
 import { extractFieldComponents } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 
 const Home = () => {
   const fields = useAppSelector(selectFields);
@@ -18,15 +19,21 @@ const Home = () => {
           <EmptyState />
         </AnimatedCard>
       ) : (
-        <AnimatedCard className="p-4 flex flex-col gap-4 overflow-y-auto">
-          {fields.map((field, index) => {
-            const PreviewComp = extractFieldComponents(field).previewComponent;
-            return (
-              <SortableField index={index} id={field.id}>
-                <PreviewComp key={field.id} config={field} />
-              </SortableField>
-            );
-          })}
+        <AnimatedCard className="p-4 flex flex-col h-full">
+          <div className="flex flex-col gap-4 overflow-y-auto flex-1 p-4">
+            {fields.map((field, index) => {
+              const PreviewComp = extractFieldComponents(field).previewComponent;
+              return (
+                <SortableField index={index} id={field.id}>
+                  <PreviewComp key={field.id} config={field} />
+                </SortableField>
+              );
+            })}
+          </div>
+
+          <div className="self-end mt-4">
+            <Button>Create Form</Button>
+          </div>
         </AnimatedCard>
       )}
     </>
